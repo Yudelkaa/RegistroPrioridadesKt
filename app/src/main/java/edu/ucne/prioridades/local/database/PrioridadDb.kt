7242f0d00acd8facc404f7ef2373c715841598ac
Yudelkaa <yudelka_guillen@ucne.edu.do>
2024-08-29 17:40:06 -0400

    Agregando database :D

---
+++ app/src/main/java/edu/ucne/prioridades/local/database/PrioridadDb.kt
@@ -0,0 +1,18 @@
+package edu.ucne.prioridades.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.prioridades.local.dao.PrioridadDao
+import edu.ucne.prioridades.local.entities.PrioridadEntity
+
+
+@Database(
+    entities = [
+        PrioridadEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class PrioridadDb : RoomDatabase() {
+    abstract fun prioridadDao() : PrioridadDao
+}
