b47793fae553b192e9855eee6fe6506ada366257
Yudelkaa <yudelka_guillen@ucne.edu.do>
2024-10-05 19:50:09 -0400

    Agrega Dto y Api

---
+++ app/src/main/java/edu/ucne/prioridades/data/remote/SistemasApi.kt
@@ -0,0 +1,27 @@
+package edu.ucne.prioridades.data.remote
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import edu.ucne.prioridades.data.remote.dto.SistemaDto
+
+interface SistemasApi {
+    @GET("api/Sistemas/{id}")
+    suspend fun getSistema(@Path("id") id: Int) : SistemaDto
+
+    @GET("api/Sistemas")
+    suspend fun getSistemas() : List<SistemaDto>
+
+    @POST("api/Ssistemas")
+    suspend fun addSistema(@Body sistemaDto: SistemaDto?): SistemaDto
+
+    @PUT("api/Sistemas/{id}")
+    suspend fun updateSistema(@Path("id") id: Int, @Body sistemaDto: SistemaDto)
+
+    @DELETE("api/Sistema/{id}")
+    suspend fun deleteSistema(@Path("id") id: Int): Response<Unit>
+}
---
+++ app/src/main/java/edu/ucne/prioridades/data/remote/dto/SistemaDto.kt
@@ -0,0 +1,6 @@
+package edu.ucne.prioridades.data.remote.dto
+
+data class SistemaDto(
+    val sistemaId : Int,
+    val nombre : String
+)
